05f05331df69874f832be0c016d4258a17ab7ed7
oldsen <2365665276@qq.com>
2023-12-27 03:38:10 +0800

    提交2`

--- rabbitmq-producer-spring/src/main/java/com/sen/rabbitmq/Pro3.java
+++ rabbitmq-producer-spring/src/main/java/com/sen/rabbitmq/Pro3.java
@@ -1,19 +1,16 @@
 package com.sen.rabbitmq;
 
 /**
- * ClassName: ProducerTest2
+ * ClassName: Pro3
  * Package: com.sen.rabbitmq
  * Description:
  *
  * @Author sen
- * @Create 2023-12-27 3:24
+ * @Create 2023-12-27 3:33
  * @Version 1.0
  */
-public class ProducerTest2 {
+public class Pro3 {
     public static void main(String[] args) {
-        System.out.println("eee");
-        System.out.println("ddd");
-
 
     }
 }
